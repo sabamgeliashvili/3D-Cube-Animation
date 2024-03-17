ef287e093f7029b82e297ec540d241e3a221fc14
sabamgeliashvili <sba69717@gmail.com>
2024-03-17 14:30:51 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Box } from '@react-three/drei';
+import './App.css'; 
+
+function SpinningCube() {
+  const cubeRef = React.useRef();
+
+  useFrame(() => {
+    cubeRef.current.rotation.x += 0.01;
+    cubeRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <Box ref={cubeRef} position={[0, 0, 0]} scale={[2, 2, 2]}>
+      <meshStandardMaterial attach="material" color="lightblue" roughness={0.5} metalness={0.5} />
+    </Box>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="info">
+        <h1>Simple Spinning Cube Animation with Three.js and React</h1>
+      </div>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} intensity={1} />
+        <SpinningCube />
+      </Canvas>
     </div>
   );
 }
